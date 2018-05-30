25e031a4c0d1fbf7759a5d058e36011ca121b123
Alexx <d5enemy@gmail.com>
2018-05-30 20:57:37 +0300

    new comment for commit test

---
+++ Lecture8_Hometask/Lecture8_Hometask/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture8_Hometask
+{
+    abstract class Elements
+    {
+        public string Name { get; set; }
+
+        public void AboutMe()
+        {
+            Console.WriteLine("Im {0}", Name);            
+        }
+    }
+
+    interface IFlyingObject
+    {
+        void Fly();
+    }
+
+    interface ISwimmingObject
+    {
+        void Swim();
+    }
+
+    interface IRunningObject
+    {
+        void Run();
+    }
+
+    interface IEnginessObject
+    {
+        void Engine();
+    }
+
+    class Airplane : Elements, IFlyingObject, IEnginessObject
+    {
+        public string Model { get; set; }
+
+        public Airplane(string model, string name)
+        {
+            this.Model = model;
+            this.Name = name;
+        }
+
+        public void Fly()
+        {
+            AboutMe();
+            Console.WriteLine("I can fly from Airplane class with IFly");
+        }
+
+        public void Engine()
+        {
+            AboutMe();
+            Console.WriteLine("I have engine from Airplane class with IEngine");
+        }
+    }
+
+    class Eagle : Elements, IFlyingObject
+    {
+        public void Fly()
+        {
+            AboutMe();
+            Console.WriteLine("I can fly from Eagle class with IFly");
+        }
+    }
+
+    class Duck : Elements, IFlyingObject, ISwimmingObject
+    {
+        public void Fly()
+        {
+            AboutMe();
+            Console.WriteLine("I can fly from Duck class with IFly");
+        }
+
+        public void Swim()
+        {
+            AboutMe();
+            Console.WriteLine("I can swim from Duck class with ISwim");
+        }
+    }
+
+    class Cheeken : Elements, IRunningObject
+    {
+        public void Run()
+        {
+            AboutMe();
+            Console.WriteLine("I can run from Duck class with IRun");
+        }
+    }
+
+    class EngineBoard : Elements, ISwimmingObject, IEnginessObject
+    {
+        public void Swim()
+        {
+            AboutMe();
+            Console.WriteLine("I can swim from EngineBoard class with ISwim");
+        }
+
+        public void Engine()
+        {
+            AboutMe();
+            Console.WriteLine("I have engine from EngineBoard class with IEngine");
+        }
+    }
+
+    class Rabbit : Elements, IRunningObject
+    {
+        public void Run()
+        {
+            AboutMe();
+            Console.WriteLine("I can run from Rabbit class with IRun");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airplane a = new Airplane("777", "Boing");
+            a.Engine();
+            a.Fly();
+
+            Eagle b = new Eagle();
+            b.Name = "Hawk";
+            b.Fly();
+
+            Duck c = new Duck();
+            c.Name = "Ducky";
+            c.Fly();
+            c.Swim();
+
+            Cheeken d = new Cheeken();
+            d.Name = "Cheek";
+            d.Run();
+
+            EngineBoard e = new EngineBoard();
+            e.Name = "Board";
+            e.Engine();
+            e.Swim();
+
+            Rabbit f = new Rabbit();
+            f.Name = "Rab";
+            f.Run();
+
+            IFlyingObject[] flying = new IFlyingObject[] { a, b, c };
+
+            foreach (var item in flying)            
+                item.Fly();
+            
+            ISwimmingObject[] swiming = new ISwimmingObject[] { c, e };
+
+            foreach (var item in swiming)            
+                item.Swim();            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lecture9_Practice/Lecture9_Practice/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Lecture9_Practice
+{
+    class DivisionException : Exception
+    {
+        public int x { get; }
+        public int y { get; }
+        public override string Message
+        {   //Переопределение свойства Message
+            get
+            {
+                return "Некорректная операция";
+            }
+        }
+        public DivisionException(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Нетипизированная коллекция - для хранения любых объектов
+            ArrayList a = new ArrayList();
+            a.Add("Computer");
+            a.Add("Programmer");
+            a.Add("C# cool");
+            a.Add(true);
+            a.Add(new DateTime(2015, 3, 20));
+            a.Add(new Object());
+            a.Add(new { Name = "Alex", LastNAme = "Rub" });
+
+            foreach (object obj in a) //тип универсальный обджект            
+                Console.WriteLine(obj);            
+
+            for (int i = 0; i < a.Count; i++)            
+                Console.WriteLine(a[i]);
+
+            //Типизированная коллекция
+            List<string> names = new List<string>();
+            names.Add("Jon Gates");
+            names.Add("Alex Rub");
+            names.Add("Sergey Bring");
+
+            foreach (string str in names)
+                Console.WriteLine(str);
+
+            //Ассоциативный массив
+            Hashtable currencies = new Hashtable();
+            currencies.Add("US", "Dollar");
+            currencies.Add("Japan", "Yen");
+            currencies.Add("Australia", "Dollar");
+            Console.WriteLine("US Currency", currencies["US"]);
+
+            //Dictionary
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("Pascal", 1968);
+            dict.Add("C", 1972);
+            dict.Add("C++", 1985);
+            dict.Add("Java", 1995);
+            dict.Add("C#", 2000);
+
+            if (dict.ContainsKey("Java"))
+            {
+                dict.Remove("Java");
+            }
+
+            foreach (KeyValuePair<string, int> entry in dict) //foreach (var entry in dict)
+            {
+                string k = entry.Key;
+                int v = entry.Value;
+                Console.WriteLine("{0} was created in {1} year", k, v);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
