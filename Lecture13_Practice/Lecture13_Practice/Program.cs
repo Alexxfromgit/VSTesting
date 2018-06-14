aae07b788e31fa6b3d0e96734be2ccc8b3c0d6cb
Alexx <d5enemy@gmail.com>
2018-06-14 18:54:08 +0300

    Ecran strings

--- Lecture13_Practice/Lecture13_Practice/Program.cs
+++ Lecture13_Practice/Lecture13_Practice/Program.cs
@@ -7,7 +7,9 @@ using System.Collections;
 
 namespace Lecture13_Practice
 {
-
+    //Файлы и работа с файлами
+    //\n - переход на новую строку
+    //\t - табуляция, отступ несколько пробелов
 
     class Program
     {
@@ -15,6 +17,23 @@ namespace Lecture13_Practice
 
         static void Main(string[] args)
         {
+            /*
+            String s1; //объект класса стринг
+            string s2; //переменная типа стринг
+
+            s1 = "\nОбычная\n\t\t\tконстанта";
+            s2 = @"\nНеобычная\n\tконстанта";
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine();
+            */
+
+
+
+
+
+
+            Console.ReadKey();
 
 
         }
