7f800b378d23688a1462bb7e8c01508ee0441d6b
Alexx <d5enemy@gmail.com>
2018-06-14 19:13:17 +0300

    using Directory class for get list of directories and file on the disk

--- Lecture13_Practice/Lecture13_Practice/Program.cs
+++ Lecture13_Practice/Lecture13_Practice/Program.cs
@@ -39,8 +39,41 @@ namespace Lecture13_Practice
                 sw.Write("The date is: ");
                 sw.WriteLine(DateTime.Now);
             }
+            try
+            {
+                //Create an instance of StreamReader to read the file
+                //the using statement also closes the StreamReader
+                using (StreamReader sr = new StreamReader("Testfile.txt"))
+                {
+                    string line;
+                    //Read and display lines from the file until the end of the file is reached
+                    while ((line = sr.ReadLine())!=null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //Let the user know what was wrong
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
 
+            //------------------------------------------------------
+            string currentFolder = Directory.GetCurrentDirectory();
+            Console.WriteLine("Текущая папка {0}", currentFolder);
 
+            var dirName = Directory.GetDirectories(@"d:\");
+            Console.WriteLine("Список директорий диска D:");
+            foreach (var item in dirName)            
+                Console.WriteLine(item);            
+
+            var fileName = Directory.GetFiles(@"d:\");
+            Console.WriteLine("Список файлов диска D:");
+            foreach (var item in fileName)            
+                Console.WriteLine(item);
+            
 
 
 
