8cf58f76f738e1390de32f39e3b9b9ab868879af
Alexx <d5enemy@gmail.com>
2018-06-18 20:27:10 +0300

    Add LINQ Practice project

---
+++ Lecture14_2_Practice/Lecture14_2_Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//LINQ
+namespace Lecture14_2_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var x = numbers
+                .Where(n => n > 0)
+                .Where(n => n % 2 == 0)
+                .Count(n => n < 10);
+
+            Console.WriteLine(x);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
