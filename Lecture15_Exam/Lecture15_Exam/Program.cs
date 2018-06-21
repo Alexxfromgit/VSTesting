41240a51dbdd343c65f4088c625372a8c888a916
Alexx <d5enemy@gmail.com>
2018-06-21 18:24:14 +0300

    Starting exam. Added using lybraries

---
+++ Lecture15_Exam/Lecture15_Exam/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Lecture15_Exam
+{
+
+
+
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Trash_1/Trash_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trash_1
+{
+    class Test
+    {
+
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            int a = 0;
+            decimal b = 0;
+            double c = 0;
+
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == c);
+            Console.WriteLine(a.Equals(c));
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
